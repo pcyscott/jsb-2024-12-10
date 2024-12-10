a2fa9b9962805aa3aed1e096cde7d1db67e6189a
pcyscott <pcyscott@gmail.com>
2024-12-10 10:45:54 +0900

    sp1

---
+++ src/main/java/com/mysite/practice/MainController.java
@@ -0,0 +1,14 @@
+package com.mysite.practice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "WELCOME TO SBB";
+    }
+}
