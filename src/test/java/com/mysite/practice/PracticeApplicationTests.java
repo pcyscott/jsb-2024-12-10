bdec2f58a7d7650c912089052a12cfc6b452daf3
pcyscott <pcyscott@gmail.com>
2024-12-10 15:54:23 +0900

    sp4

--- src/test/java/com/mysite/practice/PracticeApplicationTests.java
+++ src/test/java/com/mysite/practice/PracticeApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mysite.practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PracticeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
 }
