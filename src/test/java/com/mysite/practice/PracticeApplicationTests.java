2f5df3f6fcb9697151dfb3347198669e43623317
pcyscott <pcyscott@gmail.com>
2024-12-11 10:04:06 +0900

    sp6

--- src/test/java/com/mysite/practice/PracticeApplicationTests.java
+++ src/test/java/com/mysite/practice/PracticeApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -12,7 +16,7 @@ class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
-    @Test
+    @Test //db 에 데이터 저장
     void testJpa() {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
@@ -26,5 +30,20 @@ class SbbApplicationTests {
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
     }
+    @Test // 데이터 전부 출력
+    void testJpa2(){
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
 
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+    @Test //findbyid 메서드
+    void testJpa3(){
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
 }
