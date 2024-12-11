6c9398fff1218e5f8bb78f52628a5321c546f478
pcyscott <pcyscott@gmail.com>
2024-12-11 14:25:27 +0900

    sp11

--- src/test/java/com/mysite/practice/PracticeApplicationTests.java
+++ src/test/java/com/mysite/practice/PracticeApplicationTests.java
@@ -16,6 +16,8 @@ class SbbApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
 
     @Test
         //db 에 데이터 저장
@@ -94,6 +96,19 @@ class SbbApplicationTests {
         Question q = oq.get();
         this.questionRepository.delete(q);
         assertEquals(1, this.questionRepository.count());
+    }
+
+    @Test // 답변 데이터 저장하기
+    void testJpa9() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
     }
 }
 
